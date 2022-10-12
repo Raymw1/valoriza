95f6f41030bd9da71fcfe454f72cb4babae945d2
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-12 15:34:14 -0300

    Add TypeOrm database connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
+import 'reflect-metadata';
 import express from 'express';
 
-const app = express();
+import './database';
 
-app.get('/', (req, res) => res.send('Hello!!!!'));
+const app = express();
 
 app.listen(3000, () => console.log('Go to http://localhost:3000'));
