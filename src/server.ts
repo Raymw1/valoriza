ae5bdd6f152d08d00dec79e6bd0f3399b3c411ec
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-12 16:47:18 -0300

    Add CreateUser Controller

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import 'reflect-metadata';
 import express from 'express';
 
 import './database';
+import { router } from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('Go to http://localhost:3000'));
