c95f319abe5051651a4b554122c34ef0b39f7094
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-15 15:18:28 -0300

    Add Authenticate user controller

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,16 @@ import { Router } from 'express';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
 
 router.post('/users', createUserController.handle);
+router.post('/session', authenticateUserController.handle);
 router.post('/tags', ensureAdmin, createTagController.handle);
 
 export { router };
