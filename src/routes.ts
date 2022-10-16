a9ef6433f75fcc9b616c11a9b54cd150cbd8536b
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-16 20:14:34 -0300

    Add list user sender compliments

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { CreateUserController } from './controllers/CreateUserController';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { ListUserReceiveComplimentsController } from './controllers/ListUserReceiveComplimentsController';
+import { ListUserSenderComplimentsController } from './controllers/ListUserSenderComplimentsController';
 
 const router = Router();
 
@@ -16,6 +17,8 @@ const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
 const listUserReceiveComplimentsController =
   new ListUserReceiveComplimentsController();
+const listUserSenderComplimentsController =
+  new ListUserSenderComplimentsController();
 
 router.post('/users', createUserController.handle);
 router.post('/session', authenticateUserController.handle);
@@ -34,6 +37,11 @@ router.get(
   '/users/compliments/receive',
   ensureAuthenticated,
   listUserReceiveComplimentsController.handle
+);
+router.get(
+  '/users/compliments/send',
+  ensureAuthenticated,
+  listUserSenderComplimentsController.handle
 );
 
 export { router };
