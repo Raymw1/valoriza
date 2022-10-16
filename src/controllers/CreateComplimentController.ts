e6bf4437cedbae5c79e00180694ddda35d71e574
Rayan Wilbert <rayan.rmw@gmail.com>
2022-10-16 19:47:11 -0300

    Fixing user sender id in compliment controller

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,12 +3,12 @@ import { CreateComplimentsService } from '../services/CreateComplimentsService';
 
 class CreateComplimentController {
   async handle(request: Request, response: Response) {
-    const { message, tag_id, user_sender, user_receiver } = request.body;
+    const { message, tag_id, user_receiver } = request.body;
     const createComplimentsService = new CreateComplimentsService();
     const compliment = await createComplimentsService.execute({
       message,
       tag_id,
-      user_sender,
+      user_sender: request.user_id,
       user_receiver,
     });
     return response.status(201).json(compliment);
